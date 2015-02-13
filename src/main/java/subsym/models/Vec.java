4db52277d6cfe904cf9b7c10cc7e42461c04ad47
Patrick Skjennum <mail@habitats.no>
2015-02-13 14:29:05 +0100

    new vec with full test coverage

---
+++ src/main/java/subsym/models/Vec.java
@@ -0,0 +1,123 @@
+package subsym.models;
+
+import java.util.Arrays;
+
+/**
+ * Created by anon on 28.01.2015.
+ */
+public class Vec {
+
+  public double x;
+  public double y;
+
+  private Vec(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  // instance methods
+
+  public Vec multiply(Vec v) {
+    x *= v.x;
+    y *= v.y;
+    return this;
+  }
+
+  public Vec subtract(Vec v) {
+    x -= v.x;
+    y -= v.y;
+    return this;
+  }
+
+  public Vec divide(Vec v) {
+    x /= v.x;
+    y /= v.y;
+    return this;
+  }
+
+  public Vec add(Vec v) {
+    x += v.x;
+    y += v.y;
+    return this;
+  }
+
+  public double lenght() {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  }
+
+  public Vec normalize() {
+    return divide(lenght());
+  }
+
+  public Vec divide(double size) {
+    x /= size;
+    y /= size;
+    return this;
+  }
+
+  public Vec multiply(double o) {
+    x *= o;
+    y *= o;
+    return this;
+  }
+
+  // static methods
+
+  public static Vec add(Vec u, Vec z, Vec... vecs) {
+    Vec sum = new Vec(u.x, u.y).add(z);
+    Arrays.asList(vecs).stream().forEach(v -> sum.add(v));
+    return sum;
+  }
+
+  public static Vec subtract(Vec u, Vec z, Vec... vecs) {
+    Vec first = new Vec(u.x, u.y).subtract(z);
+    Arrays.asList(vecs).stream().forEach(v -> first.subtract(v));
+    return first;
+  }
+
+  public static Vec multiply(Vec u, Vec z, Vec... vecs) {
+    Vec sum = new Vec(u.x, u.y).multiply(z);
+    Arrays.asList(vecs).stream().forEach(v -> sum.multiply(v));
+    return sum;
+  }
+
+  public static Vec divide(Vec u, Vec z, Vec... vecs) {
+    Vec sum = new Vec(u.x, u.y).divide(z);
+    Arrays.asList(vecs).stream().forEach(v -> sum.divide(v));
+    return sum;
+  }
+
+  public static Vec divide(Vec v, double size) {
+    return new Vec(v.x / size, v.y / size);
+  }
+
+  public static Vec multiply(Vec v, double o) {
+    return new Vec(v.x * o, v.y * o);
+  }
+
+  public static double lenght(Vec v) {
+    return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
+  }
+
+  public static Vec normalize(Vec v) {
+    return divide(v, lenght(v));
+  }
+
+  @Override
+  public String toString() {
+    return "(" + x + ", " + y + ") - | " + lenght() + " |";
+  }
+
+  public static Vec create(double x, double y) {
+    return new Vec(x, y);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Vec) {
+      Vec o = (Vec) obj;
+      return o.x == x && o.y == y;
+    }
+    return false;
+  }
+}
---
+++ src/test/java/test_Vec.java
@@ -0,0 +1,118 @@
+import org.junit.Test;
+
+import subsym.models.Vec;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by Patrick on 13.02.2015.
+ */
+public class test_Vec {
+
+  @Test
+  public void test_equals() {
+    Vec v = Vec.create(1, 2);
+    Vec u = Vec.create(1, 2);
+    assertTrue(v.equals(u));
+  }
+
+  @Test
+  public void test_static_create() {
+    Vec v = Vec.create(1, 2);
+    assertEquals(v.x, 1., 0);
+    assertEquals(v.y, 2., 0);
+  }
+
+  @Test
+  public void test_static_add() {
+    Vec v = Vec.create(3, 1);
+    Vec u = Vec.create(2, 1);
+    assertTrue(Vec.add(v, u).equals(Vec.create(5, 2)));
+  }
+
+  @Test
+  public void test_static_divide() {
+    Vec v = Vec.create(9, 4);
+    Vec u = Vec.create(3, 2);
+    assertTrue(Vec.divide(v, u).equals(Vec.create(3, 2)));
+  }
+
+  @Test
+  public void test_static_subtract() {
+    Vec v = Vec.create(3, 1);
+    Vec u = Vec.create(2, 1);
+    assertTrue(Vec.subtract(v, u).equals(Vec.create(1, 0)));
+  }
+
+  @Test
+  public void test_static_multiply() {
+    Vec v = Vec.create(3, 2);
+    Vec u = Vec.create(2, 8);
+    assertTrue(Vec.multiply(v, u).equals(Vec.create(6, 16)));
+  }
+  @Test
+  public void test_static_multiply_scalar() {
+    Vec v = Vec.create(3, 2);
+    assertTrue(Vec.multiply(v, 9).equals(Vec.create(27, 18)));
+  }
+
+  @Test
+  public void test_length() {
+    Vec v = Vec.create(3, 4);
+    assertEquals(v.lenght(), 5., 0);
+  }
+
+  @Test
+  public void test_static_length() {
+    Vec v = Vec.create(3, 4);
+    assertEquals(Vec.lenght(v), 5., 0);
+  }
+
+  @Test
+  public void test_normalize() {
+    Vec v = Vec.create(3, 4);
+    v.normalize();
+    assertEquals(v.lenght(), 1., 0);
+  }
+
+  @Test
+  public void test_static_normalize() {
+    Vec v = Vec.create(3, 4);
+    assertEquals(Vec.normalize(v).lenght(), 1., 0);
+  }
+
+  @Test
+  public void test_multiply() {
+    Vec v = Vec.create(3, 2);
+    Vec u = Vec.create(2, 8);
+    assertTrue(v.multiply(u).equals(Vec.create(6, 16)));
+  }
+  @Test
+  public void test_multiply_scalar() {
+    Vec v = Vec.create(3, 2);
+    assertTrue(v.multiply(9).equals(Vec.create(27, 18)));
+  }
+
+  @Test
+  public void test_add() {
+    Vec v = Vec.create(3, 2);
+    Vec u = Vec.create(2, 8);
+    assertTrue(v.add(u).equals(Vec.create(5, 10)));
+  }
+
+  @Test
+  public void test_subtract() {
+    Vec v = Vec.create(3, 2);
+    Vec u = Vec.create(2, 8);
+    assertTrue(v.subtract(u).equals(Vec.create(1, -6)));
+  }
+
+  @Test
+  public void test_divide() {
+    Vec v = Vec.create(9, 4);
+    Vec u = Vec.create(3, 2);
+    assertTrue(v.divide(u).equals(Vec.create(3, 2)));
+  }
+}
