d7f72ac337bc9b4e0e4b3f9362667ed4e7a59d87
Habitats <mail@habitats.no>
2015-02-23 02:41:33 +0100

    bitch lolz works

--- src/main/java/subsym/lolz/LolzGenotype.java
+++ src/main/java/subsym/lolz/LolzGenotype.java
@@ -1,18 +1,61 @@
 package subsym.lolz;
 
+import java.util.BitSet;
+
 import subsym.ga.Genotype;
+import subsym.ga.Phenotype;
 
 /**
  * Created by anon on 23.02.2015.
  */
 public class LolzGenotype extends Genotype {
 
-  protected LolzGenotype(int size) {
-    super(size);
+  private LolzPhenotype phenotype;
+
+  public LolzGenotype() {
+    super();
   }
 
   @Override
-  protected Genotype newInstance(int size) {
-    return new LolzGenotype(size);
+  public Phenotype getPhenotype() {
+    return phenotype;
+  }
+
+  @Override
+  public Genotype setRandom(int size) {
+    super.setRandom(size);
+    phenotype = new LolzPhenotype(this);
+    return this;
+  }
+
+  @Override
+  public Genotype setEmpty(int size) {
+    super.setEmpty(size);
+    phenotype = new LolzPhenotype(this);
+    return this;
+  }
+
+  @Override
+  public Genotype fromString(String s) {
+    super.fromString(s);
+    phenotype = new LolzPhenotype(this);
+    return this;
+  }
+
+  @Override
+  public Genotype copy() {
+    LolzGenotype copy = (LolzGenotype) super.copy();
+    copy.phenotype = new LolzPhenotype(copy);
+    return copy;
+  }
+
+  @Override
+  public BitSet getBits() {
+    return super.getBits();
+  }
+
+  @Override
+  protected Genotype newInstance() {
+    return new LolzGenotype();
   }
 }
