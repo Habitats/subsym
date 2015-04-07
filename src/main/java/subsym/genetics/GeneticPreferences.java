ca2574da4db29c1d507f5f512eabab2dc7dcbadd
Patrick Skjennum <mail@habitats.no>
2015-04-07 16:35:55 +0200

    better tuop

--- src/main/java/subsym/genetics/GeneticPreferences.java
+++ src/main/java/subsym/genetics/GeneticPreferences.java
@@ -136,7 +136,7 @@ public class GeneticPreferences {
     GeneticPreferences prefs = new GeneticPreferences(30, 0.1, 0.9, 0.0017, new Mixing(0.5), new Rank(), 200);
     GeneticProblem problem = new AiLife(prefs, AnnPreferences.getAiLifeDefault());
     prefs.setPuzzle(problem);
-    prefs.setAnnPreferences(new AnnPreferences(0, 0, new Sigmoid()));
+    prefs.setAnnPreferences(new AnnPreferences(1, 6, new Sigmoid()));
     return prefs;
   }
 
